444ac75876dcb79bfb3445e09d0a85cf20685bd2
Amanda <ampcosta@uolinc.com>
2018-08-18 17:23:23 -0300

    separando em 5 filas

--- src/main/java/com/fiap/frameworks/clothes/async/MessageListener.java
+++ src/main/java/com/fiap/frameworks/clothes/async/MessageListener.java
@@ -22,9 +22,33 @@ public class MessageListener {
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    @JmsListener(destination = "invoice", containerFactory = "myFactory")
-    public void receiveMessage(SaleResponse sale) {
-        LOGGER.info("receiving invoice to generate pdf " + sale.getId());
+    @JmsListener(destination = "invoice1", containerFactory = "myFactory")
+    public void invoice1(SaleResponse sale) {
+        LOGGER.info("receiving invoice1 to generate pdf " + sale.getId());
+        PDFMaker.generateInvoicePDF(sale);
+    }
+
+    @JmsListener(destination = "invoice2", containerFactory = "myFactory")
+    public void invoice2(SaleResponse sale) {
+        LOGGER.info("receiving invoice2 to generate pdf " + sale.getId());
+        PDFMaker.generateInvoicePDF(sale);
+    }
+
+    @JmsListener(destination = "invoice3", containerFactory = "myFactory")
+    public void invoice3(SaleResponse sale) {
+        LOGGER.info("receiving invoice3 to generate pdf " + sale.getId());
+        PDFMaker.generateInvoicePDF(sale);
+    }
+
+    @JmsListener(destination = "invoice4", containerFactory = "myFactory")
+    public void invoice4(SaleResponse sale) {
+        LOGGER.info("receiving invoice4 to generate pdf " + sale.getId());
+        PDFMaker.generateInvoicePDF(sale);
+    }
+
+    @JmsListener(destination = "invoice5", containerFactory = "myFactory")
+    public void invoice5(SaleResponse sale) {
+        LOGGER.info("receiving invoice5 to generate pdf " + sale.getId());
         PDFMaker.generateInvoicePDF(sale);
     }
 
@@ -37,12 +61,33 @@ public class MessageListener {
 
         SaleResponse[] salesArray = gson.fromJson(sales, SaleResponse[].class);
 
-        List<SaleResponse> listResponse = Arrays.asList(salesArray);
+        if(salesArray != null){
 
-        listResponse.stream().parallel().forEach(sale -> {
-            LOGGER.info("sending invoice to queue " + sale.getId());
-            jmsTemplate.convertAndSend("invoice", sale);
-        });
+            int count = 0;
+
+            for (SaleResponse sale: salesArray) {
+                if(sale != null){
+                    LOGGER.info("sending invoice to queue " + sale.getId());
+
+                    ++count;
+
+                    if(count == 1){
+                        jmsTemplate.convertAndSend("invoice1", sale);
+                    } else if (count == 2) {
+                        jmsTemplate.convertAndSend("invoice2", sale);
+                    } else if (count == 3){
+                        jmsTemplate.convertAndSend("invoice3", sale);
+                    } else if(count == 4){
+                        jmsTemplate.convertAndSend("invoice4", sale);
+                    } else {
+                        jmsTemplate.convertAndSend("invoice5", sale);
+                        count = 0;
+                    }
+
+                }
+            }
+        }
     }
 
 }
+
